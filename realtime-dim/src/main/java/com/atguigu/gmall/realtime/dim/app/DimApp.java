c978fe6c315caf9586da6fa61660cd9446893463
admin <2973682263@qq.com>
2025-05-23 17:21:42 +0800

    Dim层开发完成

--- realtime-dim/src/main/java/com/atguigu/gmall/realtime/dim/app/DimApp.java
+++ realtime-dim/src/main/java/com/atguigu/gmall/realtime/dim/app/DimApp.java
@@ -13,33 +13,68 @@ import com.ververica.cdc.connectors.mysql.source.MySqlSource;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.common.functions.RichMapFunction;
-import org.apache.flink.api.common.restartstrategy.RestartStrategies;
-import org.apache.flink.api.common.state.BroadcastState;
 import org.apache.flink.api.common.state.MapStateDescriptor;
-import org.apache.flink.api.common.state.ReadOnlyBroadcastState;
-import org.apache.flink.api.common.time.Time;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.configuration.Configuration;
-import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.runtime.state.hashmap.HashMapStateBackend;
-import org.apache.flink.streaming.api.CheckpointingMode;
 import org.apache.flink.streaming.api.datastream.BroadcastConnectedStream;
 import org.apache.flink.streaming.api.datastream.BroadcastStream;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
-import org.apache.flink.streaming.api.environment.CheckpointConfig;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.ProcessFunction;
-import org.apache.flink.streaming.api.functions.co.BroadcastProcessFunction;
-import org.apache.flink.streaming.api.functions.sink.RichSinkFunction;
 import org.apache.flink.util.Collector;
 import org.apache.hadoop.hbase.client.Connection;
 
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.util.*;
+/*
+   开发流程
+        基本环境准备
+        检查点相关设置
+        从Kafka主题中读取数据
+        对流中的数据进行类型转换并ETL  jsonStr -> jsonObj
+        --------------------------------------------
+        使用FlinkCDC读取配置表中的配置信息
+        对读取到的配置流数据进行类型转换 jsonStr -> 实体类对象
+        根据当前配置信息到HBase中执行建表或者删除操作
+            op=d    删表
+            op=c,r  建表
+            op=u    先删表再建表
+        对配置流数据进行广播-broadcast
+        关联主流业务数据以及广播流配置数据--connect
+        对关联后的数据进行处理 --- process
+             new TableProcessFunction extends BroadcastProcessFunction{
+             open:将配置信息厦加载到程序中，避免主流数据先到、广播流数据后到，丢失数的况
+             processElement;对主流数据的处理
+                获取操作的表的表名
+                根据表名到广播状态中议及configHap中获取对应的配置信息，如果配置信息不为空，说明是维度，将维度数据发送到下游
+                   Tuple2<dataJsonObj,配置对象>
+                在向下游发送数据前，过滤掉了不需要传递的属性
+                在向下游发送数据时，补充操作类型
+            processBroadcastElement:对广播派数据进行处理
+                op =d 将配置信息从广播状态以及configMap中删除
+                op!=d 将配置信息放到广播状态以从configMap
+             }
+         将流中数据同步到HBase中
+            class HBaseSinkFunction extends RichSinkFunction{
+                invoke:
+                    type="delete":从HBase表中删除数据
+                    type!="delete":向HBase中put数据
+            }
+         优化:抽取FlinkSourceUtilL工具类
+              抽取TableProcessFunction以及HBaseSinkFunction函数处理
+              抽取方法
+              抽取基类---模板方法设计模式
+        执行流程(以修改了品牌维度表中的一条数据为例)
+            当程序启动的时候，会将配置表中的配置信息加载到configMap以及广播状态中
+            修改品牌维度
+            binlog会将修改操作记录下来
+            maxwell会从binlog中获取修改的信息，并封装为json字符串格式发送到kafka的topic_db主题中
+            DimApp应用程序会从topic_db上中读取数据并对其进行处理
+            根据当前处理内数拱府表名判断是否为维度
+            如果是维度的话，将维度数据传递到下游
+            将维度数据同步到HBase中
+
+ */
+
 
 public class DimApp extends BaseApp {
     // todo 维度层的处理
